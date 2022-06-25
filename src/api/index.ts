85c5092342b35689807ee31cf26ec95cea48d9c8
Reinaldo Silveira <reinaldopsilveira@gmail.com>
2022-06-25 17:01:19 -0300

    método post

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,19 @@
 import * as express from 'express';
+import queues from '../queues';
 const router = express.Router();
 
-const getPing = async (_req, res) => {
+const getPing = async (req, res) => {
   const body = { message: 'hello' };
   return res.send(body).status(200);
 };
 
+const postLog = async (req, res) => {
+  const body = { message: req.body.message };
+  await queues.log.add(body);
+  return res.send(body).status(200);
+};
  
 router.get('/', getPing);
+router.post('/log', postLog);
 
 export default router;
